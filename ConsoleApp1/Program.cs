83acd268c04f8cd6ecb88357d2e3da9944252e16
Roppy116 <YusupovIlF@gmail.com>
2022-03-17 12:36:40 +0300

    added result

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 30;
+            System.Console.WriteLine("Result:");
+            System.Console.WriteLine($"a * b = {a*b}");
+        }
+
+    }
+
+}
